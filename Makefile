d47e889e19f6e16d20c76a777d0a93191e290708
Moyon Enzo <moyon_e@etna-alternance.net>
2023-11-30 14:07:43 +0100

    fix make test & initialise database

--- Makefile
+++ Makefile
@@ -2,13 +2,15 @@ build:
 	docker compose build
 
 up:
-	docker compose up -d
+	docker compose up -d api
 
 down:
 	docker compose down
 
 test:
-	docker compose exec -it api pytest -v -s
+	docker compose up -d api_test && \
+	docker compose exec -it api_test pytest -v -s && \
+	docker compose down
 
 lint:
 	docker run --rm --volume ./:/src --workdir /src pyfound/black:latest_release black --check .
--- src/tests/datasets/__init__.py
+++ src/tests/datasets/__init__.py
@@ -1,1 +1,20 @@
-# TODO
+import pytest
+import src.models as models
+
+
+class Dataset:
+    def __init__(self, session):
+        self.session = session
+
+    def create_categories(self, label: str) -> models.Categories:
+        category = models.Categories(label=label)
+        self.session.add(category)
+        return category
+
+
+@pytest.fixture
+def dataset(session):
+    ds = Dataset(session)
+    ds.category_1 = ds.create_categories("first_category")
+
+    ds.session.commit()
